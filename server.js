274c37aa78c835026dfa2b4a5b618d337d33ecde
adityakumar <adityawrs1@gmail.com>
2026-01-09 12:32:55 +0530

    updated some files

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+//const express = require('express');
+
+const app = express();
+
+app.get('/',(req,res) =>{
+    res.send('Welcome for learning express js');
+});
+
+app.get('/hello',(req,res)=>{
+    res.send(`helllooo aditya!!`);
+})
+
+app.get('/healthy',(req,res)=>{
+    res.status(400).json({message:'all good!!'});
+})
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT,()=>{
+    console.log(`server is lo listening ${PORT} `)
+});
